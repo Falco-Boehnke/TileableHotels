b1ae53da4acb1f66824eb6c9fa8cee50d5044df0
Falco <falco.boehnke@hs-furtwangen.de>
2023-04-03 23:57:26 +0200

    feat: implement json fetching to generate hotel list

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import { HotelTile } from "./components/HotelTile/HotelTile";
+import { HotelData, HotelList } from "./types/hotelType";
 function App() {
+  const [hotelsList, setHotels] = useState<HotelList>({ hotels: [] });
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchHotels = async () => {
+      const hotels = (await (
+        await fetch("data/hotels.json", {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        })
+      ).json()) as HotelList;
+
+      setHotels(hotels);
+      setIsLoading(false);
+    };
+
+    fetchHotels();
+  }, []);
+
+  if (isLoading) return <h1>Loading...</h1>;
+
   return (
     <>
       <header className="header"></header>
       <div className="header__title">Mobile</div>
       <main className="content-wrapper">
         <section className="tile-list">
-          <div className="tile">
-            <div className="tile__image">
-              <img src="images/hotel-02.jpg" alt="" />
-            </div>
-            <div className="tile__title">
-              <div className="flex tile__stars">
-                <span className="tile__star" />
-                <span className="tile__star" />
-                <span className="tile__star" />
-              </div>
-              <h2>Hotel 2</h2>
-              <span className="tile__title__location">
-                Köln - 1,2 km entfernt vom Stadtzentrum
-              </span>
-            </div>
-            <div className="tile__details">
-              <div className="tile__details__capacity">
-                182 Zimmer · 12 Tagungsräume
-              </div>
-              <div className="flex tile__details__ratings">
-                <div className="flex flex--gap tile__details__hygiene">
-                  <div className="tile__details__hygiene-icon" />
-                  <span>Hygiene-Zertifikat</span>
-                </div>
-                <div className="flex flex--gap tile__details__rating">
-                  <div className="tile__details__grade">7,2</div>
-                  <span>Sehr gut</span>
-                </div>
-              </div>
-            </div>
-
-            <div className="tile__link-details">
-              <a href="#">Details ansehen</a>
-            </div>
-            <div className="tile__price_details">ab 59€</div>
-
-            <button className="tile__selection-button">
-              <span className="tile__selection-button__icon" />
-              Auswählen
-            </button>
-          </div>
+          {hotelsList.hotels.map((hotel) => {
+            return (
+              <HotelTile
+                image={
+                  (!!hotel.image && {
+                    src: hotel.image.src,
+                    alt: hotel.image.alt,
+                  }) ||
+                  undefined
+                }
+                tileTitle={{
+                  starCount: hotel.tileTitle.starCount || 0,
+                  headline: hotel.tileTitle.headline,
+                  locationLabel: hotel.tileTitle.locationLabel,
+                }}
+                tileDetails={{
+                  capacityLabel: hotel.tileDetails?.capacityLabel || "",
+                  hasHygieneCertificate:
+                    !!hotel.tileDetails?.hasHygieneCertificate,
+                  ...(hotel.tileDetails?.grade && {
+                    grade: {
+                      points: hotel.tileDetails.grade.points,
+                      label: hotel.tileDetails.grade.label,
+                    },
+                  }),
+                }}
+                detailsUrl={hotel.detailsUrl}
+                price={hotel.price}
+              />
+            );
+          })}
         </section>
       </main>
     </>
